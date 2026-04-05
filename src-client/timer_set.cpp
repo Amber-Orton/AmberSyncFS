dff1179ed5f02680a9e400fe26ccd182471e63be
amber-orton <ambertcorton@gmail.com>
2026-04-05 16:01:32 +0100

    feat: better timer set

--- src-client/timer_set.cpp
+++ src-client/timer_set.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <pthread.h>
 #include "sync_event_creator.h"
+#include <iostream>
 
 struct acess_info {
     std::chrono::steady_clock::time_point last_access_time;
@@ -14,10 +15,11 @@ struct timer_set
     std::unordered_map<std::string, acess_info> myset;
 
     // checks if an element is in the set and adds it if its not, returns 1 if present and resets elements timer, 0 otherwise and if the last event creation time was more than 60 seconds ago
-    int check_and_add(const std::string& key) {
+    int check_and_add(const std::string& event_type, const std::string& event_priority, const std::string& relative_path) {
         pthread_t cleanup_thread;
         pthread_create(&cleanup_thread, nullptr, &timer_set::cleanup_thread_func, this);
         pthread_detach(cleanup_thread);
+        std::string key = event_type + " " + event_priority + " " + relative_path;
         auto item = myset.find(key);
         if (item != myset.end()) {
             auto now = std::chrono::steady_clock::now();
@@ -44,13 +46,25 @@ struct timer_set
                 std::string event_key = item->first;
                 item = myset.erase(item);
                 if (should_create_event) {
-                    auto split_pos = event_key.find(' ');
-                    if (split_pos == std::string::npos || split_pos + 1 >= event_key.size()) {
+                    auto first_split_pos = event_key.find(' ');
+                    if (first_split_pos == std::string::npos || first_split_pos + 1 >= event_key.size()) {
+                        std::cerr << "Invalid event key format: " << event_key << "\n";
                         continue;
                     }
-                    auto event_type = event_key.substr(0, split_pos);
-                    auto relative_path = event_key.substr(split_pos + 1);
-                    create_non_priority_event_file(event_type, relative_path);
+                    auto second_split_pos = event_key.find(' ', first_split_pos + 1);
+                    if (second_split_pos == std::string::npos || second_split_pos + 1 >= event_key.size()) {
+                        std::cerr << "Invalid event key format: " << event_key << "\n";
+                        continue;
+                    }
+
+                    auto event_type = event_key.substr(0, first_split_pos);
+                    auto event_priority = event_key.substr(first_split_pos + 1, second_split_pos - first_split_pos - 1);
+                    auto relative_path = event_key.substr(second_split_pos + 1);
+                    if (event_type.empty() || event_priority.empty() || relative_path.empty()) {
+                        std::cerr << "Invalid event key format: " << event_key << "\n";
+                        continue;
+                    }
+                    create_event_file(event_type, event_priority, relative_path);
                 }
             } else {
                 ++item;
